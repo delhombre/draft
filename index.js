ce8016dc0beb27b060425434d282e0e7b2c7c2db
delhombre <brunodogbase@gmail.com>
2021-03-22 21:57:21 +0000

    adding some changes

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const selectEl = (el) => document.querySelector(el);
+const selectAllEl = (el) => document.querySelectorAll(el);
 
 selectEl(".mobile-menu").addEventListener("click", () => {
 	selectEl("header").classList.toggle("active");
 });
+
+// selectEl(".nav-link-search").addEventListener("click", () => {
+// 	selectAllEl("#item-to-hide").forEach((el) => {
+// 		el.classList.add("hide-item");
+// 		selectEl(".search-form-container").classList.add("active");
+// 	});
+// });
+
+// selectEl(".close-button").addEventListener("click", () => {
+// 	selectAllEl("#item-to-hide").forEach((el) => {
+// 		el.classList.remove("hide-item");
+// 		selectEl(".search-form-container").classList.remove("active");
+// 	});
+// });
+
+// const itemLength = selectAllEl(".hide-item").length;
+
+// selectAllEl(".hide-item").forEach((el) => {
+// 	el.style.transitionDelay = `${itemLength / 10} s`;
+
+// 	itemLength--;
+// });
